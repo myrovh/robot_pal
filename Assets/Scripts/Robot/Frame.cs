fa16d8162cbe85061eb4622db12b4237c59c6d52
myrovh <myrovh@yandex.com>
2015-09-03 22:38:00 +1000

    basic robot shape in scene

---
+++ Assets/Scripts/Robot/Frame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frame : MonoBehaviour
+{
+
+    public Vector3 headPoint;
+    public Vector3 torsoPoint;
+    public Vector3 armPoint;
+    public Vector3 locomotionPoint;
+    public Vector3 movePoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

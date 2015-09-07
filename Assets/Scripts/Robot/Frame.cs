ea668cbb6ea67abedadf618aa3243de6c18d5b10
myrovh <myrovh@yandex.com>
2015-09-08 00:49:06 +1000

    adjustments to the level graph

--- Assets/Scripts/Robot/Frame.cs
+++ Assets/Scripts/Robot/Frame.cs
@@ -13,14 +13,12 @@ public class Frame : MonoBehaviour
 
     private Seeker _seeker;
 
-	// Use this for initialization
 	public void Start ()
 	{
 	    _seeker = GetComponent<Seeker>();
         _seeker.pathCallback += OnPathComplete;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
@@ -33,11 +31,6 @@ public class Frame : MonoBehaviour
     public void RePath()
     {
         _seeker.StartPath(transform.position, movePoint);
-    }
-
-    public void SetTargetPosition(Vector3 endPosition)
-    {
-        movePoint = endPosition;
     }
 
     public void OnPathComplete(Path p)

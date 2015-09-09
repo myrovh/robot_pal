800d618b347805e862eff95b3c37b832ab338d1c
myrovh <myrovh@yandex.com>
2015-09-09 11:28:06 +1000

    robot can now follow a given path

--- Assets/Scripts/Robot/Frame.cs
+++ Assets/Scripts/Robot/Frame.cs
@@ -5,13 +5,16 @@ using Pathfinding;
 public class Frame : MonoBehaviour
 {
 
-    public Vector3 visionPoint;
-    public Vector3 facingPoint;
-    public Vector3 itemPoint;
-    public Vector3 locomotionPoint;
+    public Vector3 VisionPoint;
+    public Vector3 FacingPoint;
+    public Vector3 ItemPoint;
+    public Vector3 LocomotionPoint;
 
-    public Vector3 movePoint;
+    public Vector3 MovePoint;
     private Seeker _seeker;
+    public GameObject PropulsionPrefab;
+    public GameObject AttachedPropulsion;
+    private Propulsion _propulsionScript;
     private const float RepathTimeoutInterval = 5.0f;
     private float _repathTimeout;
 
@@ -19,10 +22,12 @@ public class Frame : MonoBehaviour
 	{
 	    _seeker = GetComponent<Seeker>();
 	    _repathTimeout = RepathTimeoutInterval;
+	    _propulsionScript = AttachedPropulsion.GetComponent<Propulsion>();
 	}
 	
 	void Update ()
 	{
+        /*
         //Force a repath every 5 seconds
 	    _repathTimeout -= Time.deltaTime;
 	    if (_repathTimeout < 0.0f)
@@ -31,21 +36,23 @@ public class Frame : MonoBehaviour
 	        _repathTimeout = RepathTimeoutInterval;
             Debug.Log(gameObject.name + " generated new path.");
 	    }
+        */
 	}
 
     public void UpdateMoveLocation(Vector3 location)
     {
-        movePoint = location;
+        MovePoint = location;
         UpdatePath();
     }
 
-    private void UpdatePath()
+    public void UpdatePath()
     {
-        _seeker.StartPath(transform.position, movePoint, OnPathComplete);
+        _seeker.StartPath(transform.position, MovePoint, OnPathComplete);
     }
 
     private void OnPathComplete(Path p)
     {
         Debug.Log("Path found. Error: " + p.error);
+        _propulsionScript.UpdateMovementLocation(p);
     }
 }
--- Assets/Scripts/Robot/Propulsion.cs
+++ Assets/Scripts/Robot/Propulsion.cs
@@ -1,8 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Pathfinding;
 
-public class Propulsion : Module {
-	void Update () {
-	
+public class Propulsion : Module
+{
+    public float NextWaypointDistance;
+
+    private Path _waypoints;
+    private int _currentWaypoint = 0;
+    private int tempSpeed = 6;
+
+	void Update ()
+	{
+        //If path exists and there are still nodes to traverse
+	    if (_waypoints == null) return;
+	    if (_currentWaypoint >= _waypoints.vectorPath.Count) return;
+
+        //Movement caluclations
+	    Vector3 direction = (_waypoints.vectorPath[_currentWaypoint] - transform.position).normalized;
+	    direction *= tempSpeed*Time.deltaTime;
+	    Move(direction);
+
+        //Advance to next node when close enough
+	    if (Vector3.Distance(transform.position, _waypoints.vectorPath[_currentWaypoint]) < NextWaypointDistance)
+	    {
+	        _currentWaypoint++;
+	    }
 	}
+
+    public void UpdateMovementLocation(Path p)
+    {
+        _waypoints = p;
+        _currentWaypoint = 0;
+    }
+
+    private void Move(Vector3 direction)
+    {
+        transform.parent.position += direction;
+    }
 }

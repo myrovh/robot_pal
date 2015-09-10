a392c145f371760488525d49127976686c0985b4
myrovh <myrovh@yandex.com>
2015-09-11 02:42:44 +1000

    robot now turns towards movement location

--- Assets/Scripts/Robot/Propulsion.cs
+++ Assets/Scripts/Robot/Propulsion.cs
@@ -7,27 +7,37 @@ public class Propulsion : Module
     public float NextWaypointDistance;
 
     private Path _waypoints;
-    private int _currentWaypoint = 0;
-    private int tempSpeed = 6;
+    private int _currentWaypoint;
+    private int tempSpeed = 6; //TODO speed is calculated from attached servos
+    private float tempTurnRate = 3.0f;
+    private float tempTurnOnlyAngle = 20.0f;
 
     private void Update()
     {
-        //only activate if can draw enough power
-        ActivatePropulsion();
+        //TODO only activate if can draw enough power
+        Move();
     }
 
-    private void ActivatePropulsion()
+    private void Move()
     {
         //If path exists and there are still nodes to traverse
         if (_waypoints == null) return;
         if (_currentWaypoint >= _waypoints.vectorPath.Count) return;
 
         //Turn Calculations
+        Vector3 direction = (_waypoints.vectorPath[_currentWaypoint] - transform.position).normalized;
+        float angle = Vector3.Angle(transform.parent.forward, direction);
+        Vector3 rotation = Vector3.RotateTowards(transform.parent.forward, direction, tempTurnRate * Time.deltaTime, 0.0f);
 
         //Forward Caluclations
-        Vector3 direction = (_waypoints.vectorPath[_currentWaypoint] - transform.position).normalized;
-        direction *= tempSpeed * Time.deltaTime;
-        Move(direction);
+        Vector3 forwardMovement = transform.parent.forward * tempSpeed * Time.deltaTime;
+
+        //Move parent robot
+        transform.parent.rotation = Quaternion.LookRotation(rotation);
+        if (angle < tempTurnOnlyAngle)
+        {
+            transform.parent.position += forwardMovement;
+        }
 
         //Advance to next node when close enough
         if (Vector3.Distance(transform.position, _waypoints.vectorPath[_currentWaypoint]) < NextWaypointDistance)
@@ -40,10 +50,5 @@ public class Propulsion : Module
     {
         _waypoints = p;
         _currentWaypoint = 0;
-    }
-
-    private void Move(Vector3 direction)
-    {
-        transform.parent.position += direction;
     }
 }
